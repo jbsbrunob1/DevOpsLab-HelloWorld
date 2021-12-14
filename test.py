ae76a3ef51aaa7b107a046ae1eaa5b9f56e04f1c
Josiney Bruno <jbs.brunob1@gmail.com>
2021-12-14 08:24:45 -0500

    Alterando o teste

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Pipeline DevOps - Bruno Santos")
+        self.assertRegex(result.data.decode(), "Pipeline nao DevOps - Bruno Santos")
 
 
 if __name__ == "__main__":
